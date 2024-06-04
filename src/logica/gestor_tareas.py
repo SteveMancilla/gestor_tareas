6490b715cbdf7780b379092b83acf9de701a5b41
Steven <72620878@continental.edu.pe>
2024-06-03 19:23:16 -0500

    Configuración inicial del proyecto GESTOR DE TAREAS

---
+++ src/logica/gestor_tareas.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class Tarea:
+    def __init__(self, titulo, descripcion):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.completada = False
+
+
+class GestorTareas:
+    pass
